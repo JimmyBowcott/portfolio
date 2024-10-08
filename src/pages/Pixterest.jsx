ac82792c1cc242d1bc3ceb7863b0c9558d817384
Jimmy Bowcott <151764691+JimmyBowcott@users.noreply.github.com>
2024-10-08 09:33:40 +0100

    update Pixterest

--- src/pages/Pixterest.jsx
+++ src/pages/Pixterest.jsx
@@ -2,11 +2,11 @@ const Pixterest = () => {
     return (
         <div className="flex flex-col justify-center w-full max-w-[1100px] gap-4 mx-4 mt-8 mb-8">
             <h1 className="text-3xl sm:text-5xl font-bold">Pixterest</h1>
-            <p><strong>Tech: </strong> MongoDB, Express, React, Node, Tailwind, Docker</p>
-            <p><strong>Github link: </strong> <a className="text-orange-500 hover:underline" href="https://github.com/jimmybowcott/pixterest"  target="_blank">https://jimmybowcott.github.io/pixterest</a></p>
+            <p><strong>Tech: </strong> MongoDB, Express, React, Node, Tailwind</p>
+            <p><strong>Live link: </strong> <a className="text-orange-500 hover:underline" href="https://pixterest.onrender.com/"  target="_blank">https://pixterest.onrender.com/</a></p>
             <p>Pixterest is a front-end Pinterest clone designed specifically for pixel art. Built with React, it features DeviantArt's RSS API to fetch posts which can then be saved by users.
-                It is designed to be fully responsive for every screen size. Additionally, the project has been upgraded to feature user login handled with Node, Express and MongoDB, and containerised
-                using Docker. My inspiration for this project came from wanting to make a challenging website with my own personal spin.</p>
+                It is designed to be fully responsive for every screen size. Additionally, the project has been upgraded to feature user login handled with Node, Express and MongoDB, and includes a containerised
+                version using Docker. My inspiration for this project came from wanting to make a challenging website with my own personal spin.</p>
             <video controls class="w-full h-auto max-w-[1100px] self-center">
                 <source src="./assets/media/pixterest.mp4" type="video/mp4" />
             </video>
